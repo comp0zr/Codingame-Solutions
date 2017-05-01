4504b8dc963ac7062fcaab1c5c330630d92f54a7
comp0zr <johncareycomposer@gmail.com>
2017-05-01 13:02:57 -0400

    Create CGXformatter.cpp
    CGX content is composed of ELEMENTs.
    
    ELEMENT
    An ELEMENT can be of any of the following types BLOCK, PRIMITIVE_TYPE or KEY_VALUE.
    
    BLOCK
    A sequence of ELEMENTs separated by the character ;
    A BLOCK starts with the marker ( and ends with the marker ).
    
    PRIMITIVE_TYPE
    A number, a Boolean, null, or a string of characters (surrounded by the marker ')
    
    KEY_VALUE
    A string of characters separated from a BLOCK or a PRIMITIVE_TYPE by the character =
     
    ---------------------------------
    Your mission: write a program that formats CGX content to make it readable!
    
    Beyond the rules below, the displayed result should not contain any space, tab, or carriage return. No other rule should be added.​
    
    The content of strings of characters must not be modified.
    A BLOCK starts on its own line.
    The markers at the start and end of a BLOCK are in the same column.
    Each ELEMENT contained within a BLOCK is indented 4 spaces from the marker of that BLOCK.
    A VALUE_KEY starts on its own line.e.
    A PRIMITIVE_TYPE starts on its own line unless it is the value of a VALUE_KEY.
    INPUT:
    Line 1: The number N of CGX lines to be formatted
    The N following lines: The CGX content. Each line contains maximum 1000 characters. All the characters are ASCII.
    
    OUTPUT:
    The formatted CGX content
    
    CONSTRAINTS :
    The CGX content supplied will always be valid.
    The strings of characters do not include the character '
    0 < N < 10000

---
+++ hard/CGXformatter.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+string invalid = "()\t ";
+vector<string> CGX;
+
+bool quote = false;
+int index;
+int indentLevel = 0;
+
+void Reformat(string s)
+{
+    string str = "";
+    
+    for(int i=0; i<s.length(); i++)
+    {
+        char c = s[i];
+        string indent(indentLevel*4, ' ');
+        
+        if(c == '\\' && s[i+1] == 't')
+        {
+            str += "    ";
+            i++;
+            continue;
+        }
+        
+        if(quote)
+        {
+            str += c;
+            if(c != '\'') continue;
+        }
+        else if(invalid.find(c) == string::npos) str += c;
+        
+        
+        switch(c)
+        {
+            case '\'':
+            {
+                if(str.empty())
+                {
+                    cout << indent << str;
+                    str.clear();
+                    str = "\'";
+                }
+                quote = !quote;
+                break;
+            }
+            
+            case '(':
+            {
+                if(!str.empty())
+                {
+                    cout << indent << str << endl;
+                    str.clear();
+                }
+                indentLevel++;
+                cout << indent << '(' << endl;
+                break;
+            }
+            
+            case ')':
+            {
+                if(!str.empty())
+                {
+                    cout << indent << str << endl;
+                    str.clear();
+                }
+                indentLevel--;
+                
+                if(indent.length() > 4) indent = indent.substr(4);
+                else indent.clear();
+                
+                if(i < s.length()-1)
+                {
+                    size_t found = s.find_first_not_of(" ", i+1);
+                    
+                    if(s[found] == ';')
+                    {
+                        cout << indent << ");" << endl;
+                        i = s.find_first_not_of(" ", i+1);
+                    }
+                    else cout << indent << ')' << endl;
+                }
+                else cout << indent << ')' << endl;
+                
+                break;
+            }
+            
+            case ';':
+            {
+                cout << indent << str << endl;
+                str.clear();
+                break;       
+            }
+        }
+    }
+    string indent(indentLevel*4, ' ');
+    
+    if(!str.empty()) 
+    {
+        cout << indent << str;
+        
+        if(str.back() != '=') cout << endl;
+        else if (index < CGX.size()-1)
+        {
+            string next = CGX[index+1];
+            size_t found = next.find_first_not_of(" ");
+            if(next[found] == '(') cout << endl;
+        }
+    }
+}
+
+
+int main()
+{
+    int N;
+    cin >> N; cin.ignore();
+    CGX.resize(N);
+    
+    for (int i = 0; i < N; i++) 
+    {
+        string CGXLine;
+        getline(cin, CGXLine);
+        CGX[i] = CGXLine;
+    }
+    for(auto s : CGX)
+    {
+        Reformat(s);
+        index++;
+    }
+}
